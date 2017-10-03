a88cacd7f1450a4f1a12b09097b12409b2e408b9
Claudemir Modkowski <claudemirmod@outlook.com>
2017-10-03 10:41:28 -0300

    Alteração no Startup

--- Teste/Teste/Startup.cs
+++ Teste/Teste/Startup.cs
@@ -29,6 +29,7 @@ namespace Teste
         {
             // Add framework services.
             services.AddMvc();
+            //Ok
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
